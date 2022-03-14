dc670d797e2d3ad9b4c2055ca060e130be8e093a
Yousef Drgham <youdr728@student.liu.se>
2022-03-14 19:17:15 +0100

    jumpBoost

---
+++ src/se/liu/joeri765youdr728/Platformer/JumpBoost.java
@@ -0,0 +1,11 @@
+package se.liu.joeri765youdr728.Platformer;
+
+public class JumpBoost extends AbstractEntity
+{
+    public JumpBoost(final int x, final int y, final int typeNumber, final int collisionX, final int collisionY, final int collisionWidth,
+		     final int collisionHeight)
+    {
+	super(x, y, typeNumber, collisionX, collisionY, collisionWidth, collisionHeight);
+    }
+
+}
